91908ccbd133adc44d7f47c4c145d12fc71679f5
dmk28 <daniel.kik.adv@protonmail.com>
2023-04-01 23:38:42 -0300

    reading Sylvain Kerkour's Black Hat Rust

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std:: {env, error::Error, fs::File, io::{BufRead, BufReader}};
+use sha1::Digest;
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+fn main() -> Result<(), Box<dyn Error>>{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Usage:\n");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return Ok(());
+    }
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let common_password = line.trim();
+        if hash_to_crack == 
+            &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+                println!("Password found: {}", &common_password);
+                return Ok(());
+            }
+        println!("{}", line);
+    }
+    println!("Password not found in wordlist");
+
+    Ok(())
+
+}
