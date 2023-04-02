58ec4c9c33d40608c0e956bc35992df97900fbc3
dmk28 <daniel.kik.adv@protonmail.com>
2023-04-01 23:38:49 -0300

    Go

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std:: {env, error::Error, fs::File, io::{BufRead, BufReader}};
 use sha1::Digest;
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
@@ -22,15 +27,13 @@ fn main() -> Result<(), Box<dyn Error>>{
     for line in reader.lines() {
         let line = line?;
         let common_password = line.trim();
-        if hash_to_crack == 
-            &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
-                println!("Password found: {}", &common_password);
-                return Ok(());
-            }
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+            println!("Password found: {}", &common_password);
+            return Ok(());
+        }
         println!("{}", line);
     }
     println!("Password not found in wordlist");
 
     Ok(())
-
 }
